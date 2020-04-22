ded0caf909661abaf751127b983dd91d7e7282fc
TIKER504 <lin791126@gmail.com>
2020-04-22 18:35:34 +0800

    2020/04/22 TIKER  新增 依據螢幕大小 自動切換介面， 且簡略支援手機屏幕遊玩

--- snake2player.js
+++ snake2player.js
@@ -3,8 +3,13 @@
 var snake_head_class = get_rule('.snake_head') 
 var snake_head = [get_rule('#snake_head_0'),get_rule('#snake_head_1')]
 var playground = get_rule('#playground')
-var playground_w = parseInt(playground.style.width)
-var playground_h = parseInt(playground.style.height)
+// var playground_w = parseInt(playground.style.width)
+// var playground_h = parseInt(playground.style.height)
+
+// 為了響應 螢幕長寬 CSS 自動識別，先暫且硬寫
+var playground_w = 1000;
+var playground_h = 800;
+
 
 var x;
 var y;
@@ -32,6 +37,8 @@ var pre_move = [[],[]];
 
 var playing = false;
 
+var restarted =false;
+
 //聲音設定
 
 //開場音樂
@@ -57,6 +64,17 @@ eatsound.src="resource/audio/MarioCoin.mp3";
 var gameoversound = new Audio();
 gameoversound.src="resource/audio/gameoverBGM.mp3";
 
+
+// 檢測是否為行動裝置
+var isMobile = false; //initiate as false
+// device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+	isMobile = true;
+	
+	
+
+}
 
 
 
@@ -239,7 +257,7 @@ function move_oneway_up(player){
 	direction[player] = 38
 }
 
-
+// PC 電腦鍵盤 鍵入 監聽事件
 addEventListener("keydown", keboard_control);
 function keboard_control(event){
 	switch(event.which){
@@ -284,6 +302,40 @@ function keboard_control(event){
 		pre_control(1,event.which)	
 	}
 }
+
+// 手機螢幕  觸碰 監聽事件
+document.getElementById("upTouchBtn").addEventListener("touchend",touch_control);
+document.getElementById("downTouchBtn").addEventListener("touchend",touch_control);
+document.getElementById("leftTouchBtn").addEventListener("touchend",touch_control);
+document.getElementById("rightTouchBtn").addEventListener("touchend",touch_control);
+
+document.getElementById("phoneStartBtn").addEventListener("touchend",touch_control);
+
+function touch_control(event)
+{
+	switch(event.srcElement.id){
+		case "upTouchBtn":
+			// 方向鍵上 Code 為38
+			pre_control(0,38)
+			break;
+		case "downTouchBtn":
+			// 方向鍵下 Code 為40
+			pre_control(0,40)
+			break;
+		case "leftTouchBtn":
+			// 方向鍵左 Code 為37
+			pre_control(0,37)
+		case "rightTouchBtn":
+			// 方向鍵右 Code 為39
+			pre_control(0,39)
+			break;		
+		case "phoneStartBtn":
+		// 空白鍵 Code 為32
+		pause();
+		break;		
+	}
+}
+
 
 
 function creat_food(){
@@ -334,6 +386,12 @@ function pause(player){
 	// 按下空白建後，將所有的Focus 取消掉，可以避免 若原本僅要移動人物，卻誤觸使控制面板設定跑掉，遊戲機制重啟的問題
 	document.activeElement.blur();
 	
+	// 假如沒有重新設置， 就主動重來
+	if(!restarted)
+	{	
+		restart();
+	}
+
 	playing = true;
 
 	if(pause_b)
@@ -410,6 +468,8 @@ function restart(){
 	
 	opensound.currentTime =0;
 	opensound.play();
+
+	restarted = true;
 }
 function remove_snake_body(player){
 	var length = snake_body_array[player].length
@@ -579,6 +639,7 @@ function dead_gray(player){
 	// 撞擊死亡後。更換頭像
 	document.getElementById("snake_head_"+player).setAttribute("style", "background-image: url('resource/image/RabitNo.gif')")
 	
+	restarted = false;
 }
 
 function pre_control(player,keycode){	
